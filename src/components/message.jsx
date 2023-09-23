541e98c3c524d220c02e1853c2b66a7be969ddea
Bochkovskyi <unundertale1@ukr.net>
2023-09-23 17:23:46 +0300

    Add message component

---
+++ src/components/message.jsx
@@ -0,0 +1,25 @@
+import { timeFormatter } from '@/utils/getFormattedDate'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Message({ sentAt, children }) {
+  const { format: formatTime } = timeFormatter()
+
+  return (
+    <div className="flex items-end gap-2">
+      <Link className="rounded-full overflow-hidden" href="/">
+        <Image
+          className="object-center object-cover"
+          src="/icons/avatar.svg"
+          width={38}
+          height={38}
+          alt="Picture of the author"
+        />
+      </Link>
+      <div className="flex justify-between gap-2 rounded-lg rounded-bl-none w-fit max-w-md py-2 px-3 bg-orange-100">
+        <p className="break-words">{children}</p>
+        <span className="text-xs self-end">{formatTime(sentAt)}</span>
+      </div>
+    </div>
+  )
+}
