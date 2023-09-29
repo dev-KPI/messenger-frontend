181081f8da891017d9f43a5daac92f09d4237023
Bochkovskyi <unundertale1@ukr.net>
2023-09-29 19:18:30 +0300

    add app theme colors

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import './global.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex flex-col bg-slate-300">
+      <body className="bg-lightAccent dark:bg-darkAccent flex flex-col bg-slate-300">
         <main>{children}</main>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
+  darkMode: 'class',
   theme: {
+    colors: {
+      light: '#fff',
+      lightAccent: '#efeff3',
+      dark: '#18323b',
+      darkAccent: '#102024',
+      primary: '#00c2ed',
+    },
     extend: {},
   },
   plugins: [],
