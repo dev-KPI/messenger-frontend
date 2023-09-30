33626e70b33fce9517d52cff64192f2eece8907a
Bochkovskyi <unundertale1@ukr.net>
2023-09-30 16:51:21 +0300

    add theme context

---
+++ src/store/theme-context.jsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { createContext, useEffect, useState } from 'react'
+
+export const ThemeContext = createContext({
+  theme: 'light',
+  toggleTheme: () => {},
+})
+
+export default function ThemeProvider({ children }) {
+  const [currentTheme, setCurrentTheme] = useState('light')
+
+  useEffect(() => {
+    checkTheme()
+  }, [currentTheme])
+
+  const checkTheme = () => {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      localStorage.setItem('theme', 'dark')
+      document.documentElement.classList.add('dark')
+      setCurrentTheme('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+      setCurrentTheme('light')
+    }
+  }
+
+  const toggleThemeHandler = () => {
+    const theme = localStorage.getItem('theme')
+
+    if (theme) {
+      localStorage.setItem('theme', theme === 'dark' ? 'light' : 'dark')
+    } else {
+      localStorage.setItem('theme', 'dark')
+    }
+
+    setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <ThemeContext.Provider
+      value={{ theme: currentTheme, toggleTheme: toggleThemeHandler }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  )
+}
