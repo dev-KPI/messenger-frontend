901deff3452f64eae8a4ae30168f3c8056dd6ac6
Bochkovskyi <unundertale1@ukr.net>
2024-01-04 18:53:07 +0200

    add auth page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,19 @@
+import AuthForm from '@/components/Pages/Auth/AuthForm'
+import ImageCard from '@/components/Pages/Auth/ImageCard'
+import Image from 'next/image'
+
+export default function Auth() {
+  return (
+    <section className="h-screen w-screen flex items-center justify-center bg-gradient-blue-white">
+      <div className="card p-10 flex gap-[3.75rem] bg-white">
+        <ImageCard className="-my-[4.375rem] flex-shrink-0">
+          <Image alt="Golub" height={161} src="/images/golub.png" width={196} />
+        </ImageCard>
+        <div>
+          <h1 className="title-lg">Authentication</h1>
+          <AuthForm />
+        </div>
+      </div>
+    </section>
+  )
+}
