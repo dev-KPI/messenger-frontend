d7d47f656ab96ff05eb3cccfc79dcdc43525090e
Bochkovskyi <unundertale1@ukr.net>
2024-01-16 10:34:57 +0200

    utilize IconGolub component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,27 +1,22 @@
 import FormAuth from '@/components/Pages/Auth/FormAuth'
 import ImageCard from '@/components/Pages/Auth/ImageCard'
-import Container from '@/components/UI/Container'
-import Image from 'next/image'
+import IconGolub from '@/components/UI/Icons/IconGolub'
+import Layout from '@/components/UI/Layout'
 
 export default function Auth() {
   return (
     <section className="h-screen w-screen bg-gradient-blue-white">
-      <Container className="flex items-center justify-center">
+      <Layout className="flex items-center justify-center">
         <div className="card p-7 sm:p-10 flex gap-7 md:gap-[3.75rem] bg-white">
           <ImageCard className="hidden sm:block -my-[4.375rem] flex-shrink-0">
-            <Image
-              alt="Golub"
-              height={161}
-              src="/images/golub.png"
-              width={196}
-            />
+            <IconGolub />
           </ImageCard>
           <div>
             <h1 className="title-lg">Authentication</h1>
             <FormAuth />
           </div>
         </div>
-      </Container>
+      </Layout>
     </section>
   )
 }
