deae06a73be3dde7cd756dd58168114fc1b9c389
Bochkovskyi <unundertale1@ukr.net>
2024-01-16 17:49:49 +0200

    update styles

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,7 +9,7 @@ export default function Auth() {
     <section className="h-screen w-screen bg-gradient-blue-white">
       <Layout className="flex items-center justify-center dark:bg-none dark:bg-base-gray-8">
         <div className="card p-7 sm:p-10 flex gap-7 md:gap-[3.75rem] bg-base-white dark:bg-base-black">
-          <ImageCard className="hidden sm:block -my-[4.375rem] flex-shrink-0">
+          <ImageCard className="-my-[4.375rem] flex-shrink-0 hidden sm:flex items-center justify-center">
             <IconGolub />
           </ImageCard>
           <div>
