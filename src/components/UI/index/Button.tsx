2c20864ef71bfd7d7b8f14526ab9fb6d896ec761
Melnyk Mykhailo <m.melnyk.ia12@kpi.ua>
2024-01-19 21:47:42 +0200

    move old components to index folder

---
+++ src/components/UI/index/Button.tsx
@@ -0,0 +1,40 @@
+import { classnames } from '@/utils'
+import { FC, ReactNode } from 'react'
+
+type ButtonProps = {
+  children: ReactNode
+  className?: string
+  disabled?: boolean
+  onClick?: () => void
+  type?: 'button' | 'reset' | 'submit'
+}
+
+const THEMES = {
+  dark: 'disabled:bg-none disabled:text-base-gray-8 disabled:bg-base-gray-7',
+  light: 'disabled:bg-none disabled:text-base-gray-4 disabled:bg-base-gray-3',
+}
+
+const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  disabled,
+  onClick,
+  type,
+}) => {
+  return (
+    <button
+      className={classnames(
+        'inline-block py-3.5 px-5 text-center text-base-white rounded-2xl bg-gradient-purple-blue',
+        THEMES['light'],
+        className
+      )}
+      disabled={disabled}
+      onClick={onClick}
+      type={type || 'button'}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
