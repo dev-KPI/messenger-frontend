c583997fc771582077da0b51a62b25ebdbca5321
Bochkovskyi <unundertale1@ukr.net>
2024-01-23 21:41:40 +0200

    clear main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-import IconChange from '@/components/ui/icons/IconChange'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className="flex items-baseline gap-5">
-      <div className="flex flex-col items-center">
-        This is Image:{' '}
-        <Image
-          alt="change"
-          className="fill-blue-500"
-          height={24}
-          src="/icons/change.svg"
-          width={24}
-        />
-      </div>
-      <div className="flex flex-col items-center">
-        This is an IconChange component:{' '}
-        <IconChange
-          className="text-base-gray-1 hover:text-bright-orange transition-colors"
-          size="lg"
-        />
-        <p>Hover me!</p>
-      </div>
-      <div>GOLUB</div>
-      <div>🚀</div>
+    <div className="w-full h-screen flex items-center justify-center text-3xl">
+      GOLUB 🚀
     </div>
   )
 }
