41bf67199d076255d8e89f1f1153ed8bb61f7699
Bochkovskyi <unundertale1@ukr.net>
2024-01-23 17:43:01 +0200

    update imports

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,13 +1,13 @@
-import FormAuth from '@/components/pages/auth/FormAuth'
-import ImageCard from '@/components/pages/auth/ImageCard'
+import FormAuth from '@/components/pages/auth/form-auth'
+import ImageCard from '@/components/pages/auth/image-card'
+import Content from '@/components/ui/content'
 import IconGolub from '@/components/ui/icons/IconGolub'
 import ButtonThemeToggle from '@/components/ui/index/ButtonThemeToggle'
-import Layout from '@/components/ui/index/Layout'
 
 export default function Auth() {
   return (
     <section className="h-screen w-screen bg-gradient-blue-white">
-      <Layout className="flex items-center justify-center dark:bg-none dark:bg-base-gray-8">
+      <Content className="flex items-center justify-center dark:bg-none dark:bg-base-gray-8">
         <div className="card p-7 sm:p-10 flex gap-7 md:gap-[3.75rem] bg-base-white dark:bg-base-black">
           <ImageCard className="-my-[4.375rem] flex-shrink-0 hidden sm:flex items-center justify-center">
             <IconGolub />
@@ -18,7 +18,7 @@ export default function Auth() {
             <ButtonThemeToggle />
           </div>
         </div>
-      </Layout>
+      </Content>
     </section>
   )
 }
