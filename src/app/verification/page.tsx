19d8d31f562bb0ba75e7ee06ef5fe52b2cbe2495
Anton Bochkovskyi <98012691+firehawk89@users.noreply.github.com>
2024-01-31 19:42:04 +0200

    Add Verification Page (#101)
    PR: https://github.com/dev-KPI/messenger-frontend/pull/101

--- src/app/verification/page.tsx
+++ src/app/verification/page.tsx
@@ -1,21 +1,21 @@
-import FormAuth from '@/components/pages/auth/form-auth'
 import AuthContent from '@/components/pages/auth-content'
+import ControlOtp from '@/components/pages/verification/control-otp'
 import { Icons } from '@/components/ui/icons'
 import ImageCard from '@/components/ui/image-card'
 import { ThemeToggler } from '@/components/ui/theme-toggler'
 
-export default function Auth() {
+export default function Verification() {
   return (
     <AuthContent>
       <ImageCard className="-my-[4.375rem] hidden sm:flex">
         <Icons.golub />
       </ImageCard>
       <article>
-        <header className="flex justify-between pb-2">
-          <h1 className="title-lg">Authentication</h1>
+        <header className="flex justify-between items-center">
+          <h1 className="title-lg">Check your Email</h1>
           <ThemeToggler />
         </header>
-        <FormAuth className="mt-5" />
+        <ControlOtp className="mt-5" />
       </article>
     </AuthContent>
   )
